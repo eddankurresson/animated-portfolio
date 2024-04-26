317f46627eee53961d1988e5da4861353819a817
eddankurresson <104197104+eddankurresson@users.noreply.github.com>
2024-04-26 11:34:01 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import "./app.scss";
+import Navbar from "./componentsnavbar/Navbar"
+import Sidebar from "./sidebar/sidebar";
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section id="Hem">
+    
+      <Navbar/>
+
+    </section>
+
+    <section><a href="">Hello</a></section>
+    <section id="Tjänster">Parralax</section>
+    <section id="Portfolio">Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section id="Kontakt">Contact</section>
+    <section id="Om">Om</section>
+    </div>;
 };
 
 export default App;
